434e8a2b7887fdacdad4f1c57f78d45898397bfb
KiruiTito <titokirui402@gmail.com>
2023-05-21 11:55:25 +0300

    Completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+  
+    def initialize(word)
+      @word = word.downcase
+    end
+  
+    def match(anagrams)
+      anagrams.select { |anagram| anagram.downcase.chars.sort == word.chars.sort }
+    end
+  end
+  
